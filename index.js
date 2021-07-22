2e4d392772466f0d35c5cba5e1ee32db1b2807ff
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-07-22 07:10:37 +0100

    added google oauth

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
-
 const PORT = process.env.PORT || 5000;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecrete,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, "accessToken");
+      console.log(refreshToken, "refreshToken");
+      console.log(profile, "profile");
+      console.log(done, "done");
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
 
 app.listen(PORT);
