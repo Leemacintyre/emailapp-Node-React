dcf57a43e30ee07e99ad356854e6083aed21111a
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-07-23 06:37:52 +0100

    added google auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +23,21 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, "accessToken");
-      console.log(refreshToken, "refreshToken");
-      console.log(profile, "profile");
-      console.log(done, "done");
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // we already have a user with the given profileID
+            done(null, existingUser);
+          } else {
+            // we don't have a user with this ID
+            new User({ googleId: profile.id })
+              .save()
+              .then((user) => done(null, user));
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   )
 );
