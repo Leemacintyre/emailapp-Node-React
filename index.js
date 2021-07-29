99fc77339f12f90d71a363cca58893b4067ecc57
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-07-29 06:49:31 +0100

    added billing using strip

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -24,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
