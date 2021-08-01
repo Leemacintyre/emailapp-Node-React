4ab7ba6938150d0d2da8a2242bae5df723550e44
Leemacintyre <64503108+Leemacintyre@users.noreply.github.com>
2021-08-01 09:02:16 +0100

    added ability to send emails to back end

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+// Dev only axios helpers
+import axios from "axios";
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
@@ -16,3 +20,11 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
+// const survey = {
+//   title: "my title",
+//   subject: "my subject",
+//   recipients: "tempzaq123@gmail.com",
+//   body: "this is my email body",
+// };
+// axios.post("api/surveys", survey);
